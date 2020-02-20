6a3c96640831adf43774740c665962c7b161e921
vhirgo <septe.1991@gmail.com>
2020-02-20 23:57:44 +0700

    fix Login Endpoind

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,36 @@
-// conts;
+const jwt = require("jsonwebtoken");
+const User = require("../models").user;
 
-exports.index = (req, res) => {
-  res.send("Login controller");
+exports.index = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const Email = await User.findOne({
+      where: { email: email }
+    });
+    if (Email != null) {
+      const Password = await User.findOne({
+        where: { password: password }
+      });
+      if (Password != null) {
+        // console.log(Password);
+        const token = jwt.sign({ userId: password }, "my-secret-key");
+        res.send({
+          status: 200,
+          message: "success",
+          email,
+          token
+        });
+      } else {
+        res.send({
+          status: 404,
+          message: "Password Not Found"
+        });
+      }
+    }
+    res.send({
+      status: 404,
+      message: "Email Not Found"
+    });
+  } catch (error) {}
 };
