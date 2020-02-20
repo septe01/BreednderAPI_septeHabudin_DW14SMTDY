3c1ed14ada2b70c7426991af34c5e567cee51a5f
vhirgo <septe.1991@gmail.com>
2020-02-20 15:43:26 +0700

    installing mysql

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // instantiate express module
 const express = require("express");
+// initiate body-parser
+const bodyParser = require("body-parser");
+require("express-group-routes");
 
 // use express in app variable
 const app = express();
@@ -7,8 +10,28 @@ const app = express();
 // define the server port
 const port = 5001;
 
+// allow this app to receive incoming json request
+app.use(bodyParser.json());
+
+// import controller
+const LoginController = require("./controllers/login");
+
+// -- create groups routes
+// -- Login
+app.group("/api/v1", router => {
+  //strukturing folder
+  router.get("/login", LoginController.index);
+});
+
+// app.group("/api/v1", router => { //using group routes
+//   router.get("/login", (req, res) => {
+//     res.send("hallo");
+//   });
+// });
+
 // create the homepage route
 app.get("/", (req, res) => {
+  //create route endpoind
   res.send("hallo septehabudin");
 });
 
